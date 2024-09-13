bf33cf0c9609aeceeffef9ee44ee48603e1c8a4b
Angelo.Crisci <angelocrisci1998@gmail.com>
2024-09-13 18:11:35 +0200

    Più Bonus

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <script setup>
 import AppTitle from './AppTitle.vue'
+import AppHero from './AppHero.vue'
 </script>
 
 <template>
   <div>
     <AppTitle/>
   </div>
+  <div>
+    <AppHero/>
+  </div>
 </template>
 
 <style scoped>
-
+    div {
+      color: papayawhip;
+    }
 </style>
---
+++ src/AppHero.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    name: 'AppHero'
+}
+</script>
+
+<template>
+  <h2>
+    Ciao Sono un Eroe
+  </h2>
+</template>
+
+<style scoped>
+</style>
